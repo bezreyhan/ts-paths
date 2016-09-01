206eeb8e7ea222b8df1e02ca1bf790d7740603c1
Bez Reyhan <bezreyhan@gmail.com>
2016-09-01 09:30:29 -0700

    first commit

---
+++ src/first/second/third/page.js
@@ -0,0 +1,8 @@
+"use strict";
+const addTwo_1 = require('addTwo');
+function Page() {
+    return addTwo_1.default() + 3;
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = Page;
+//# sourceMappingURL=page.js.map
---
+++ src/first/second/third/page.ts
@@ -0,0 +1,5 @@
+import addTwo from 'addTwo';
+
+export default function Page() {
+  return addTwo() + 3;
+}
